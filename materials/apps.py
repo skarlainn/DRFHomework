260a3607a094649bf644aad94bc2749634b2a332
Diana <ddmitrieva098@example.com>
2026-06-10 20:42:54 +1100

    добавлены урлы

--- materials/apps.py
+++ materials/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class LmsConfig(AppConfig):
+class MaterialsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'materials'
